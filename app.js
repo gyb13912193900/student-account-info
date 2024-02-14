b6efa48eacb41b402fb1041d626a5594a494e6d5
Marcus_Gu <109802075+gyb13912193900@users.noreply.github.com>
2024-02-14 13:51:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const studentInfo = {
     name: 'Yubin Gu',
     id: 34441646,
     balance: 25,500.00,
-    bench fee: 5000.00
+    benchfee: 5000.00
 };
 
 document.getElementById('student-name').textContent = `Name: ${studentInfo.name}`;
 document.getElementById('student-id').textContent = `ID: ${studentInfo.id}`;
 document.getElementById('balance').textContent = `Balance: $${studentInfo.balance.toFixed(2)}`;
+document.getElementById('bench-fee').textContent = `Bench Fee: $${studentInfo.BenchFee.toFixed(2)}`;
